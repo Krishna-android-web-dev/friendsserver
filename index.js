5dfe3139a50457a444ae81ac53f4fbc711139f70
KRISHNA KUMAR <KRISHNA.EDU.HUB@GMAIL.COM>
2022-08-16 22:01:03 +0530

    f d

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use("/api/addfriends",friends);
 
-// krishna
+// krishna deployed
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
